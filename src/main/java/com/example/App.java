c344855e526f09edee45d4690488c8f6071617f1
roopalimule <roopalimule555@gmail.com>
2026-06-18 21:51:26 +0530

    Configure ChromeDriver with ChromeOptions
    Updated ChromeDriver initialization to use ChromeOptions for headless mode and other arguments.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,10 +3,17 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com");
         driver.manage().window().maximize();
